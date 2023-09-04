20cb3d512801fa3b7914e3aae7e6879e3ffa8dc8
LalamaniWise <ramarulalamani1@gmail.com>
2023-09-04 05:55:50 -0700

    Add project files.

---
+++ LeaveManagerTest5/Controllers/AccountController.cs
@@ -0,0 +1,68 @@
+﻿using LeaveManagerTest5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace LeaveManagerTest5.Controllers
+{
+
+
+
+        public class AccountController : Controller
+        {
+            // GET: Account
+            public ActionResult Login()
+            {
+                return View();
+            }
+
+            [HttpPost]
+            public ActionResult Login(Models.Identity model)
+            {
+                using (var context = new LeaveSystemDatabaseEntities())
+                {
+                    bool isValid = context.Users.Any(x => x.Username == model.Username && x.Password == model.password);
+                    if (isValid)
+                    {
+                        FormsAuthentication.SetAuthCookie(model.Username, false);
+                        return RedirectToAction("Index", "Employees");
+                    }
+                    ModelState.AddModelError("", "Invalid credentials");
+                    return View();
+                }
+
+            }
+
+            public ActionResult Signup()
+            {
+                return View();
+            }
+
+      
+
+        [HttpPost]
+            public ActionResult Signup(User model)
+            {
+                using (var context = new LeaveSystemDatabaseEntities())
+                {
+                    context.Users.Add(model);
+                    context.SaveChanges();
+                   
+            }
+           
+            return RedirectToAction("Login");
+
+        }
+    
+        public ActionResult Logout()
+            {
+                FormsAuthentication.SignOut();
+                return RedirectToAction("Login");
+            }
+
+        }
+   
+}
---
+++ LeaveManagerTest5/Models/Identity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LeaveManagerTest5.Models
+{
+    public class Identity
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string password { get; set; }
+    }
+}
